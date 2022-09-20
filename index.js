5cd1bee169c041a6b71227ecc1bc615ec9f41eb5
ahmed rashid <ahmeddmrashid@gmail.com>
2022-09-20 23:37:22 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate"){
+
+    // activity = typeof activity !== "undefined" ? activity : "roller-skate";
+    return `This Saturday, I want to ${activity}!`
+}
+    saturdayFun("football")
+
+
+
+const mondayWork = function (study = "go to the office"){
+    // study = typeof study !== "undefined" ? study : "go to the office";
+
+    return `This Monday, I will ${study}.`
+}
+mondayWork("code")
+
+// function wrapAdjective(special = "a dedicated programmer"){
+//     return function(pretty = "a hard worker"){
+//         return `You are *${pretty}*!`
+//     }
+//     return `You are ||${special}||!`
+// }
+// wrapAdjective()
+
+let wrapAdjective = function(style="*") {
+
+    return function(adjective="special"){
+        return `You are ${style}${adjective}${style}!`
+    }
+
+}
